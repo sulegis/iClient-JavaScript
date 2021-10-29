3061f40851ead7933c22ed15ca04fc4bc12c3880
Le <sulele@supermap.com>
2021-10-29 15:23:49 +0800

    Update karma.conf.js

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -67,7 +67,28 @@ module.exports = function (config) {
       /***common的源码***/
       '../src/common/**/*.js',
       /**测试文件**/
-      './test-main-common.js'
+      './test-main-common.js',
+
+      /***leaflet的源码***/
+      { pattern: './libs/workers/TurfWorkerForTest.js', include: false },
+      { pattern: '../node_modules/leaflet/dist/leaflet.css', include: false },
+      { pattern: '../src/leaflet/**/**/*.css', include: false },
+      '../src/leaflet/**/!(index).js',
+      /**测试文件**/
+      './test-main-leaflet.js',
+
+      /***openlayers的源码***/
+      { pattern: '../node_modules/ol/ol.css', include: false },
+      { pattern: '../src/openlayers/**/**/*.css', include: false },
+      '../src/openlayers/**/!(index).js',
+      /**测试文件**/
+      './test-main-openlayers.js',
+
+      /***mapboxgl***/
+      { pattern: '../node_modules/mapbox-gl/dist/mapbox-gl.css', include: false },
+      '../src/mapboxgl/**/!(index).js',
+      /**测试文件**/
+      './test-main-mapboxgl.js'
     ],
 
     // list of files to exclude 测试时排除的文件
