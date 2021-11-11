a852e21c9226e5c8f467ca9d564e9855103e95d6
Le <sulele@supermap.com>
2021-11-11 22:39:26 +0800

    Create test-main-leaflet-mini.js

---
+++ test/test-main-leaflet-mini.js
@@ -0,0 +1,11 @@
+/*leaflet -- control*/
+import './leaflet/control/ChangeTileVersionSpec.js';
+
+/*leaflet -- core*/
+import './leaflet/core/NonEarthCRSSpec.js';
+import './leaflet/core/TransformUtilSpec.js';
+import './leaflet/core/Proj4LeafletSpec.js';
+
+/*leaflet -- mapping*/
+import './leaflet/mapping/ImageMapLayerSpec.js';
+import './leaflet/mapping/TiledMapLayerSpec.js';
